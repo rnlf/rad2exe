abbc56706b26ae88d102a42851d946cd3ffb1656
Florian Kesseler <flo+gitlab@base13.de>
2019-03-28 15:41:05 +0100

    Added environment sanity check

--- rad2exe.sh
+++ rad2exe.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+# Sanity check
+if [ -z "$WATCOM" ]; then
+  echo '$WATCOM not set.'
+  exit 1
+fi
+
+if [ -z "$(which wcl)" ]; then
+  echo "Watcom's wcl not found in \$PATH"
+  exit 1
+fi
+
+
 if [[ $# < 2 ]]; then
   echo "usage: $0 <rad-file> <out-file>" >&2
   exit 1
